17c2cb8b864db5a492f3751c81d01f11cd027c18
Pavel Kosach <kosachdev@gmail.com>
2023-01-31 16:10:43 +0300

    Выполнено задание к 4 уроку

--- script.js
+++ script.js
@@ -1,42 +1,20 @@
 "use strict";
-// Переменная lang может принимать 2 значения: 'ru' 'en'.
-let lang = "ru";
-let rusWeek = "Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье.";
-let engWeek = "Monday, tuesday, wednesday, thursday, friday, saturday, sunday";
 
-// Написать условия при котором в зависимости от значения lang будут выводится дни недели на русском или английском языке.
-// a) через if,
- if (lang == "ru") {
-  console.log(rusWeek);
- } else if (lang == "en") {
-  console.log(engWeek);
- } else {
-   console.log("Значение lang не соответствует en/ru");
- } 
+let checker = "Индекс, с которого начинать извлечение (нумерация начинается с нуля). Если аргумент отрицателен, то трактуется как str.length + beginIndex (например, если beginIndex равен -3, то он трактуется как str.length - 3). Если beginIndex не является числом при проверке Number(beginIndex), он трактуется как 0.";
 
-// b) через switch-case
-switch (true) {
-  case lang == "ru":
-    console.log(rusWeek);
-    break;
-  case lang == "en":
-    console.log(engWeek);
-    break;
-  default:
-    console.log("Значение lang не соответствует en/ru");
+function checkString (str) {
+  if (typeof str !== "string") {
+    console.log("передана не строка");
+  } else if (typeof str == "string") {
+    str.trim();
+  } else {
+    console.log("smth went wrong");
+  }
+
+  if (str.length > 30) {
+    str = str.slice(0, 30);
+    console.log(str + "...");
+  }
 }
 
-// c) через многомерный массив без ифов и switch.
-let dayArray = {
-  ru: ["Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье."],
-  en: ["Monday, tuesday, wednesday, thursday, friday, saturday, sunday"], 
-}
-console.log(dayArray[lang][0]);
-
-
-// У нас есть переменная namePerson.
-// Если значение этой переменной “Артем” то вывести в консоль “директор”, если значение “Александр” то вывести в консоль “преподаватель”, 
-// с любым другим значением вывести в консоль “студент”
-// Решить задачу с помощью нескольких тернарных операторов, без использования if или switch
-let namePerson = "Александр";
-namePerson == "Артем" ? console.log("директор") : namePerson == "Александр" ? console.log("преподаватель") : console.log("студент");
+checkString (checker);
